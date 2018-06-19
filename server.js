c15af3cb8e330027dd4bbe9e2bfe5d4ae52745b2
Michael Huang <huang.zha@husky.neu.edu>
2018-06-19 19:43:58 -0400

    Angular Heroku Deploy Fixes

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static('./dist/angularjs-summer2018-huang-zhao'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
+  res.sendFile(path.join(__dirname,'/dist/angularjs-summer2018-huang-zhao/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
